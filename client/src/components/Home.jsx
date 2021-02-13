44a95344c566b06efd2f37e0b3e940c51f260b3e
Logan Rosenlund <loganrosenlund61@gmail.com>
2021-02-13 15:40:27 -0800

    Refactored modals

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,23 +5,25 @@ function Home(props) {
 
   let [ newVehicle, setNewVehicle ] = useState("2015 Ford F-150");
   let [ miles, setMiles ] = useState("");
+  const [ showOdometerModal, setShowOdometerModal ] = useState(false);
+  const [ showVehicleModal, setShowVehicleModal ] = useState(false);
 
-  function handleViewChange(event) {
-    props.onViewChange(event.target.innerText.toLowerCase());
+  function handleViewChange(e) {
+    props.onViewChange(e.target.innerText.toLowerCase());
   }
 
-  function handleChange(event) {
-    console.log(event.target.value);
-    setNewVehicle(event.target.value);
+  function handleChange(e) {
+    setNewVehicle(e.target.value);
   }
 
-  function handleVehicleChange(event) {
-    event.preventDefault();
+  function handleVehicleChange(e) {
+    e.preventDefault();
     props.updateCurrent(newVehicle);
     setNewVehicle("2015 Ford F-150")
   }
 
-  function handleOdometerUpdate(event) {
+  function handleOdometerUpdate(e) {
+    e.preventDefault();
     props.updateOdometer(miles);
     setMiles("");
   }
@@ -34,6 +36,28 @@ function Home(props) {
     console.log("Add a maintenance item has been clicked");
   }
 
+  function closeSelectVehicleModal(e) {
+    e.preventDefault();
+    toggleVehicleModal();
+    handleVehicleChange(e);
+  }
+
+  function closeOdometerModal(e) {
+    e.preventDefault();
+    handleOdometerUpdate(e);
+    toggleOdometerModal();
+  }
+  
+  const handleKeyPress = (e) => {
+    e.preventDefault();
+    if(e.key === "Enter") {
+      handleOdometerUpdate(e);
+    }
+  }
+
+  const toggleOdometerModal = () => setShowOdometerModal(!showOdometerModal);
+  const toggleVehicleModal = () => setShowVehicleModal(!showVehicleModal);
+
   return (
     <div class="home">
       <div class="header">
@@ -41,82 +65,64 @@ function Home(props) {
       </div>
       <hgroup class="subheader">
         <h2 class="selected" > Home </h2>
-        <h2 class="mileage" onClick={handleViewChange}> Mileage </h2>
-        <h2 class="account" onClick={handleViewChange}> Account </h2>
+        <h2 class="mileage" onClick={handleViewChange} > Mileage </h2>
+        <h2 class="account" onClick={handleViewChange} > Account </h2>
       </hgroup>
       <div>
         <div class="row">
-          <h3>
-            Vehicle: {props.current.name}
-          </h3>
-          <Modal
-            activator={({ setShow }) => (
-              <small
-                class="small"
-                type="button"
-                onClick={() => setShow(true)}
-              >
-                change
-              </small>
-            )}
-            >
-            <form>
-              <label class="slct-vhcl">
-                <h5>
-                  Select vehicle:
-                </h5>
-                <select onChange={handleChange}>
-                  {props.user.vehicleList.map((vehicle) => {
-                    return <option value={vehicle}> {vehicle} </option>
-                  })}
-                </select>
-              </label>
-            </form>
-            <button onClick={handleVehicleChange} > SUBMIT </button>
-          </Modal>
+          <h3> Vehicle: {props.current.name} </h3>
+          <button onClick={toggleVehicleModal}> change </button>
+          {
+            showVehicleModal ? (
+              <Modal>
+                <form>
+                  <label class="slct-vhcl">
+                    <h5> Select vehicle: </h5> 
+                      <select onChange={handleChange}>
+                        {props.user.vehicleList.map((vehicle) => {
+                          return <option value={vehicle}> {vehicle} </option>
+                        })}
+                      </select> 
+                  </label>
+                </form>
+                <button onClick={closeSelectVehicleModal}> Select </button>
+                <button onClick={toggleVehicleModal} > Cancel </button>
+              </Modal>
+            ) : null}
         </div>
-        <div class="row">
-          <h3>
-            Odometer: {Intl.NumberFormat('en-us').format(props.current.odometer)} miles
-          </h3>
-          <Modal
-            activator={({ setShow }) => (
-              <small
-                class="small"
-                type="button"
-                onClick={() => setShow(true)}
-              >
-                update
-              </small>
-            )}
-          >
-            <form>
-              <label>
-                <h5>
-                  Current miles:
-                </h5>
-                <input type="text" value={miles} onChange={handleOdometerChange} />
-              </label>
-            </form>
-            <button onClick={handleOdometerUpdate} > UPDATE </button>
-          </Modal>
+        <div  class="row">
+          <h3> Odometer: {Intl.NumberFormat('en-us').format(props.current.odometer)} miles </h3>
+          <button onClick={toggleOdometerModal}> update </button>
+          {
+            showOdometerModal ? (
+              <Modal>
+                <form>
+                  <label>
+                    <h5> Current miles: </h5>
+                    <input type="text" value={miles} onChange={handleOdometerChange} onKeyPress={handleKeyPress} /> 
+                  </label>
+                </form>
+                <button onClick={closeOdometerModal}> Update </button>
+                <button onClick={toggleOdometerModal}> Cancel </button>
+              </Modal>
+            ) : null}
         </div>
-      </div>
-      <button onClick={addMaintenanceItem}> ADD MAINTENANCE ITEM </button>
-      <div class="home-body">
-        <h4>
-          UPCOMING MAINTENANCE:
-        </h4>
-        {props.current.data.map((item) => {
-          if (item.due_mileage > props.current.odometer) 
-          return <div>
-                   <div class="maitna"> <bold> Maintenance item: </bold> {item.desc} </div>
-                   <div class="maitda"> Due in: <bold> {Intl.NumberFormat('en-us').format(item.due_mileage - props.current.odometer)} </bold>  miles </div>
-                 </div>
-        })}
+
+        <button onClick={addMaintenanceItem}> ADD MAINTENANCE ITEM </button>
+        <div class="home-body" >
+          <h4> UPCOMING MAINTENANCE: </h4>
+          {props.current.data.map((item, key) => {
+            if (item.due_mileage > props.current.odometer) 
+            return <div>
+                <div class="maitna"> <bold> Maintenance item: </bold> {item.desc} </div>
+                <div class="maitda"> <bold> {Intl.NumberFormat('en-us').format(item.due_mileage - props.current.odometer)} </bold> miles </div>
+              </div>
+          })}
+        </div>
       </div>
     </div>
   )
+
 }
 
 export default Home;
